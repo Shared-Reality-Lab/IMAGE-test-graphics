3cb728bee9d9b90dd46115792a27158bd6807b33
rianadutta <riana.dutta@mail.mcgill.ca>
2022-08-12 09:42:19 -0400

    copy override instead of moving

--- azure.sh
+++ azure.sh
@@ -3,5 +3,5 @@
 
 /var/docker/atp/restoreunstable
 cd /home/rianad/test_graphics
-mv /home/rianad/test_graphics/azure-override/docker-compose.override.yml /var/docker/atp
+cp /home/rianad/test_graphics/azure-override/docker-compose.override.yml /var/docker/atp
 cd /var/docker/atp ; docker-compose up -d
