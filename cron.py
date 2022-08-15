30d51a5ce83e680bd168db8cfa6a2193d76292da
rianadutta <riana.dutta@mail.mcgill.ca>
2022-08-15 14:19:09 -0400

    Delete cron.py

--- cron.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-os.system("cd /home/rianad/test_graphics")
-os.system("cd /home/rianad/test_graphics ; echo | date >> /home/rianad/test_graphics/diff.txt")
-os.system("cd /home/rianad/test_graphics ; ./testset.py -t daily -d --daily >> /home/rianad/test_graphics/diff.txt")
-
-
-
