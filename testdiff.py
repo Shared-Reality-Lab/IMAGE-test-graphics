df4cedf09fbff338e0ed3108d8dd8df162f1fc2c
rianadutta <riana.dutta@mail.mcgill.ca>
2022-08-16 18:09:07 -0400

    sort through different keys in diff.py

--- testdiff.py
+++ testdiff.py
@@ -8,39 +8,43 @@ import argparse
 from datetime import datetime
 import requests
 import difflib
-#from jsoncomparision import Compare, NO_DIFF
-#from PIL import Image
-
-#can easily modify this to compare more than 2 time stamps in a directory, except what would we be comparing?
-#every single file compared to each other?
-
 
 #identifies differences for graphics for 2 different time stamps
 # -n specifies the number of the directory
 # if -n flag is not provided, then iterates through the entire directory to find directories that have that timestamp
-# -t followed by 2 or more time stamps
-# if all arguments aren't present, it returns an error
+# -t followed by 2 time stamps
+# --preprocessor followed by the names of the preprocessors to compare. if not specified, compares all preprocessors.
+# if -t arguments aren't present, it returns an error
 
-#creating parsers for arguments
-
-
-
-#TODO add keys to exclude compare
-
+#changes boolean keeps track of if there has been a change
 
 changes = False
+
+#creating parsers for arguments
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-n", type=int, nargs=1, help ="-t followed by two time stamps (m_d_Y_H_M_S)")
 parser.add_argument("-t", required=True, nargs=2, help = "(optional) -n followed by the integer number of the directory where the graphic is stored. If not provided, then compares all graphics that have the 2 time stamps")
-parser.add_argument("--preprocessor", help = "followed by a list of the preprocessors for which we want to see the diffs, everything else will be excluded")
+parser.add_argument("--preprocessor", nargs = '+', help = "followed by a list of the preprocessors for which we want to see the diffs, everything else will be excluded")
 
 args = parser.parse_args()
+
+#if --preprocessor was specified, keeps track of which ones need to compare in a list, toparse
+toparse = []
+preprocessorspecified = False
+if args.preprocessor:
+    preprocessorspecified = True
+    for item in args.preprocessor:
+        toparse.append("ca.mcgill.a11y.image.preprocessor." + item)
+    
+
+#ensures 2 different time stampes
 
 if args.t[0] == args.t[1]:
     assert False, "please enter two different time stamps to compare"
     
+#keeps track of which files to compare
 timestamps= args.t
 outputfiles = []
 for time in timestamps:
@@ -77,6 +81,7 @@ else:
         assert False, "no directory exists with both time stamps"
 print(file_name)
 
+#compares files for each file in the list file_name
 for file1 in file_name:
     preprocessors = {}
     outputs[0] = os.path.join(file1,outputfiles[0])
@@ -91,36 +96,28 @@ for file1 in file_name:
     pre1 = json.loads(((data0["preprocessors"]).replace("contentCategoriser\"", "contentCategoriser"))[:-1])
     pre2 = json.loads(((data1["preprocessors"]).replace("contentCategoriser\"", "contentCategoriser"))[:-1])
 
+    if not args.preprocessor:
+        for key in pre1:
+            if not pre1[key] == pre2[key]:
+                print(key)
+                print(pre1[key])
+                print(key)
+                print(pre2[key])
+                changes = True
+    else:
+        for key in toparse:
+            if key not in pre1:
+                print(key + " not a preprocessor")
+            else:    
+                if not pre1[key] == pre2[key]:
+                    print(key)
+                    print(pre1[key])
+                    print(key)
+                    print(pre2[key])
+                    changes = True
 
-    for key in pre1:
-        if not pre1[key] == pre2[key]:
-            print(key)
-            print(pre1[key])
-            print(key)
-            print(pre2[key])
-            changes = True
 
 if changes:
     print("Changes occured")
 
-   # if "daily" in data0:
-    #    data0.pop("daily")
-   # if "daily" in data1:
-   #     data1.pop("daily")
 
- #   j = json.dumps(data0)
-  #  f = open("1.json", 'x')
-  #  print(j, file=f)
-
- #   k = json.dumps(data1)
- #   l = open("2.json", 'x')
- #   print(k, file=l)
-
- #   print("for "+file1+":")
-
-  #  os.system("diff 1.json 2.json")
-  #  os.remove("1.json")
-  #  os.remove("2.json")
-
-  #  if data0 == data1:
-   #     print("same preprocessor outputs")
