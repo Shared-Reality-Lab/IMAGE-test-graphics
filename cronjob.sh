7aaf96c83b843f113bcdc8f950b2f4ccf5172b61
rianadutta <riana.dutta@mail.mcgill.ca>
2022-08-19 14:33:26 -0400

    dated diff files

--- cronjob.sh
+++ cronjob.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 cd /home/rianad/test_graphics 
-cd /home/rianad/test_graphics ; echo | date >> /var/docker/atp/testing/diff.txt
+now=`date +"%m_%d_%Y"`
 cd /home/rianad/test_graphics ; var=$(./testset.py -t daily -d --daily)
-echo $var >> /var/docker/atp/testing/diff.txt
+
 
 sub="Changes"
 
 if [[ $var == *"$sub"* ]];
 then
     echo "Changes occured"
+    echo $var >> /var/docker/atp/testing/$now.txt
     
 
 fi
