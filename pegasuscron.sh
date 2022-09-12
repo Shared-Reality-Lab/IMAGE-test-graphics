ccd2cb68433b2a1db557b76a31830df8ac480afe
rianadutta <riana.dutta@mail.mcgill.ca>
2022-09-12 00:10:54 -0400

    daily testing for pegasus

---
+++ pegasuscron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/rianad/test_graphics 
+now=`date +"%m_%d_%Y"`
+cd /home/rianad/test_graphics ; var=$(./testset.py -p -t daily -d --daily)
+
+
+sub="Changes"
+
+if [[ $var == *"$sub"* ]];
+then
+    echo "Changes occured"
+    echo $var >> /var/docker/atp/testing/$now.txt
+    
+
+fi
